d129ae8de383a6de66a9520402b9af7b0acc7007
Mirza4Dev <mirza.durakovic94@gmail.com>
2024-01-12 12:13:15 +0100

    prova

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
+      <span>ciao</span>
     </div>
   );
 }
