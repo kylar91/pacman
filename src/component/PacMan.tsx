e52cae674a2bea0e6aac724dee0ac5033c6b9408
Mirza4Dev <mirza.durakovic94@gmail.com>
2024-01-18 09:28:30 +0100

    pacman moviment

--- src/component/PacMan.tsx
+++ src/component/PacMan.tsx
@@ -26,10 +26,8 @@ const PacMan: React.FC<Props> = ({
   animation,
   setAnimation,
 }) => {
-
   const handleKeyDown = (e: React.KeyboardEvent) => {
     e.preventDefault();
-
 
     let newRow = position.row;
     let newCol = position.col;
@@ -51,26 +49,64 @@ const PacMan: React.FC<Props> = ({
         return;
     }
 
-    if (field[newRow][newCol] !== 4) {
-      setDirection(e.key)
-      handlePacManMove(newRow, newCol);
-      setAnimation(true)
-    }
+    movePacMan(newRow, newCol, e.key);
+  };
+
+  const movePacMan = (row: number, col: number, direction: string) => {
+    const isValidMove = () => {
+      return field[row][col] !== 4;
+    };
+
+    const move = () => {
+      if (isValidMove()) {
+        setDirection(direction);
+        handlePacManMove(row, col);
+        setAnimation(true);
+
+        // Prossima posizione
+        let newRow = row;
+        let newCol = col;
+
+        switch (direction) {
+          case "ArrowUp":
+            newRow -= 1;
+            break;
+          case "ArrowDown":
+            newRow += 1;
+            break;
+          case "ArrowLeft":
+            newCol -= 1;
+            break;
+          case "ArrowRight":
+            newCol += 1;
+            break;
+          default:
+            break;
+        }
+
+        if (isValidMove()) {
+          setTimeout(() => {
+            movePacMan(newRow, newCol, direction);
+          }, 200);
+        } else {
+          setAnimation(false);
+        }
+      }
+    };
+
+    move();
   };
 
   const handlePacManMove = (row: number, col: number) => {
-
     setTimeout(() => {
       const newField = [...field];
       newField[position.row][position.col] = 0;
-
-      newField[row][col] = 7;
-
-      setField(newField);
       setPosition({ row: row, col: col });
-      setAnimation(false)
-    }, 200)
-
+      newField[row][col] = 7;
+      setField(newField);
+      console.log(field);
+      setAnimation(false);
+    }, 200);
   };
 
   useEffect(() => {
@@ -81,16 +117,26 @@ const PacMan: React.FC<Props> = ({
     };
   }, []);
 
-  return <div
-    className="pacman"
-    style={{
-      ...(animation && { animation: `Animation-${direction} 0.2s steps(1)` }),
-      ...(direction === 'ArrowUp' && { transform: 'scale(1.8) rotate(270deg)' }),
-      ...(direction === 'ArrowDown' && { transform: 'scale(1.8) rotate(90deg)' }),
-      ...(direction === 'ArrowLeft' && { transform: 'scale(1.8) rotate(180deg)' }),
-      ...(direction === 'ArrowRight' && { transform: 'scale(1.8) rotate(0deg)' })
-    }}
-  ></div>;
+  return (
+    <div
+      className="pacman"
+      style={{
+        ...(animation && { animation: `Animation-${direction} 0.2s steps(1)` }),
+        ...(direction === "ArrowUp" && {
+          transform: "scale(1.8) rotate(270deg)",
+        }),
+        ...(direction === "ArrowDown" && {
+          transform: "scale(1.8) rotate(90deg)",
+        }),
+        ...(direction === "ArrowLeft" && {
+          transform: "scale(1.8) rotate(180deg)",
+        }),
+        ...(direction === "ArrowRight" && {
+          transform: "scale(1.8) rotate(0deg)",
+        }),
+      }}
+    ></div>
+  );
 };
 
 export default PacMan;
